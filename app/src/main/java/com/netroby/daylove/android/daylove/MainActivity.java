7a09c9ba657949ce02335fb7a4a2add7ac010cf0
ZhiFeng Hu <hufeng1987@gmail.com>
2016-11-19 10:50:56 +0800

    Add create button open new Activity
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- app/src/main/java/com/netroby/daylove/android/daylove/MainActivity.java
+++ app/src/main/java/com/netroby/daylove/android/daylove/MainActivity.java
@@ -1,5 +1,6 @@
 package com.netroby.daylove.android.daylove;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,8 +24,9 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Toast.makeText(getApplicationContext(), "Go create blog", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, CreateActivity.class);
+                startActivity(intent);
             }
         });
     }
